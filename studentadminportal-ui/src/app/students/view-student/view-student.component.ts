f8963191ab74f52bf1d74237f324f7d66f2436c8
chuma <nwaigbochukwuma6@gmail.com>
2022-09-28 23:28:55 +0100

    delete

--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
 import { GenderService } from 'src/app/services/gender.service';
@@ -39,7 +39,8 @@ export class ViewStudentComponent implements OnInit {
   constructor(private readonly studentService: StudentService,
     private readonly route: ActivatedRoute,
     private readonly genderService: GenderService,
-    private snackbar: MatSnackBar) { }
+    private snackbar: MatSnackBar,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
@@ -78,6 +79,25 @@ export class ViewStudentComponent implements OnInit {
         //Log it
       }
     );
+  }
+
+  onDelete(): void{
+    this.studentService.deleteStudent(this.student.id)
+    .subscribe(
+      (successResponse) => {
+        this.snackbar.open("Student Deleted Successfully", undefined, {
+          duration: 1000
+        });
+
+        setTimeout(() => {
+          this.router.navigateByUrl('students');
+        }, 1000);
+      },
+
+      (errorResponse) => {
+
+      }
+    )
   }
 
 }
